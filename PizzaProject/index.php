c30fa41ca895534254dfe12077b7792c2707fe4d
Zachary <Zcollins@petrichor.io>
2015-05-18 21:30:02 -0400

    test post var

--- PizzaProject/index.php
+++ PizzaProject/index.php
@@ -1,16 +1,35 @@
 <!DOCTYPE HTML>
-<h5>Order?</h5>
-<body>
-    <form method="post">
-        <input type="text" name="order">
-        <input type="submit" value="Order">
-        <br />
-    </form>
-</body>
+<html>
+    <h5>Order?</h5>
+    <body>
+        <?php if(empty($_POST)) { ?>
+            <form method="post">
+                <input type="text" name="order">
+                <input type="submit" value="Order">
+                <br />
+            </form>
+        <?php } else {
+            echo $_POST['order'];
+
+        } ?>
+    </body>
 </html>
 
 <?php
-
+/*
 if(!empty($_POST)) {
     echo $_POST['order'];
 }
+
+    $test = array(
+        'test' => 123,
+        'dog' => 'hi',
+        15 => 'number'
+    );
+
+    $test[15];
+*/
+
+
+
+
