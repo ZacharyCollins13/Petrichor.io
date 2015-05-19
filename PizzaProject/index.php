dffd1f86331c0245493b584b3a34c260de90c851
Zachary <Zcollins@petrichor.io>
2015-05-18 21:10:11 -0400

    auto deploy test

--- PizzaProject/index.php
+++ PizzaProject/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE HTML>
 <form>
     Order?<br>
-    <input type="text" name="">
-    <input type="submit" value="Order">
-    <?php echo "name" ?>
+    <input type="text" name="order">
+    <input type="submit" value="Order"><br>
+    <?php echo name ?>
 </form>
 </html>
